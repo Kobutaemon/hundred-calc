dcd6b113ce8f602ea666b94e900130bd84bbb6a5
Kobutaemon <isotoa1219@gmail.com>
2025-04-07 23:40:59 +0900

    generateQuestion関数を書いてる

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -3,22 +3,27 @@ import "./Result.css"
 import Footer from '../Footer/Footer'
 import { useLocation } from 'react-router'
 
-// function generateRandomNumber() {
-//   return Math.floor(Math.random() * 10) //Math.random()で0から1の間のランダムな数値を生成したものを、Math.floorでそれ以下の整数にする 
-// }
-
-// function generateQuestion() {
-//   // ランダム生成の数字を配列に入れ、それをmap関数で取り出す
-
-// }
+function generateRandomNumber() {
+  return Math.floor(Math.random() * 10) //Math.random()で0から1の間のランダムな数値を生成したものを、Math.floorでそれ以下の整数にする 
+}
 
 function Result() {
   const location = useLocation()
   const count = location.state
 
+  function generateQuestion(): React.ReactNode {
+    // ランダム生成の数字を配列に入れ、それをmap関数で取り出す
+    for ( let n = 0; n < count; n++) {
+      const i: number[] = []
+    }
+    return
+  }
+
   return (
-    <div className='Result'>
-      {count}
+    <div className='result-container'>
+      <div className="result">
+        { generateQuestion() }
+      </div>
       <Footer />
     </div>
   )
