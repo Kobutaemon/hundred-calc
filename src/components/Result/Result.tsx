b569571fc5d9291ad2bf06005500784e25b9b02f
Kobutaemon <ceti1241010@gn.iwasaki.ac.jp>
2025-04-08 14:56:05 +0900

    マップ関数実装中

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -3,26 +3,25 @@ import "./Result.css"
 import Footer from '../Footer/Footer'
 import { useLocation } from 'react-router'
 
-function generateRandomNumber() {
-  return Math.floor(Math.random() * 10) //Math.random()で0から1の間のランダムな数値を生成したものを、Math.floorでそれ以下の整数にする 
-}
 
 function Result() {
-  const location = useLocation()
-  const count = location.state
+  const location = useLocation();
+  const count = location.state;
+  const questionOption: number[] = []
 
-  function generateQuestion(): React.ReactNode {
-    // ランダム生成の数字を配列に入れ、それをmap関数で取り出す
-    for ( let n = 0; n < count; n++) {
-      const i: number[] = []
-    }
-    return
+  for (let n = 0; n < count; n++) {
+    const randomNumber = Math.floor(Math.random() * 10);
+    questionOption[n] += randomNumber
   }
 
   return (
     <div className='result-container'>
       <div className="result">
-        { generateQuestion() }
+        <table>
+          <tr>
+            {questionOption.map(i => <td key={}>{i}</td>)}
+          </tr>
+        </table>
       </div>
       <Footer />
     </div>
