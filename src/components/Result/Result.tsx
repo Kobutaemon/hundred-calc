0bd921f2b3b3abc018d1eb9d076e5c03cde6953a
Kobutaemon <ceti1241010@gn.iwasaki.ac.jp>
2025-04-11 12:52:35 +0900

    とりあえず表示まで出来た。

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -11,17 +11,16 @@ function Result() {
   for(let questionNumber = 0; questionNumber < count; questionNumber++) { //問題数のfor文
     for (let n = 0; n < 10; n++) { //10x10を作るためのfor文
       const randomNumber = Math.floor(Math.random() * 10);
-      questionOption[n] += randomNumber
+      questionOption[n] = randomNumber
     }
   }
-  console.log(questionOption.map(i => {i}))
 
   return (
     <div className='result-container'>
       <div className="result">
         <table>
           <tr>
-            {/* {questionOption.map(i => <td>{i}</td>)} */}
+            {questionOption.map(i => <td>{i}</td>)}
           </tr>
         </table>
       </div>
