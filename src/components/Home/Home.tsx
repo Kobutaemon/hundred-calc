ceb22a8835919bc312f9ab5166b3cef815a1c485
Kobutaemon <ceti1241010@gn.iwasaki.ac.jp>
2025-04-16 16:51:05 +0900

    四則演算の選択肢実装中。Resultが表示されない問題。

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,24 +1,35 @@
 import React, { useState } from 'react'
 import "./Home.css"
+// import ChoiceArithmetic from '../ChoiceArithmetic/ChoiceArithmetic'
 import { useNavigate } from 'react-router'
 import { Button } from '@mui/material'
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 interface countProps {
   count: number,
   setCount: React.Dispatch<React.SetStateAction<number>>,
 }
 
-function Home({count, setCount}: countProps) {
-  
+function Home({ count, setCount }: countProps) {
+
   const navigate = useNavigate()
   const [isComposing, setIsComposing] = useState(false);
+  const [arithmetic, setArithmetic] = useState("+");
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setArithmetic(event.target.value as string);
+  };
 
   function checkUpper() { //countの値が100以内かチェック
     if (count > 100) {
       alert("生成問題数の上限は100問です。")
     }
     else {
-      navigate("/result", {state: count})
+      navigate("/result", { state: { count: count, arithmetic: arithmetic } })
     }
   }
 
@@ -36,28 +47,36 @@ function Home({count, setCount}: countProps) {
           <div className="counter">
             <label htmlFor="counter-input">
               問題数：
-              <input 
+              <input
                 type='number'
                 className='counter-number no-spin'
                 value={count}
-                onChange={(e) => {setCount(Number(e.target.value))}}
+                onChange={(e) => { setCount(Number(e.target.value)) }}
                 id='counter-input'
                 onKeyDown={handleKeyDown}
-                onCompositionStart={() => {setIsComposing(true)}}
-                onCompositionEnd={() => {setIsComposing(false)}}
-                placeholder='上限100'
+                onCompositionStart={() => { setIsComposing(true) }}
+                onCompositionEnd={() => { setIsComposing(false) }}
               />
               <p className='upper-limit'>※上限100</p>
             </label>
-            <label htmlFor="choice-arithmetic">
-              選択肢：
-              <select id="choice-arithmetic">
-                <option value="plus">＋</option>
-                <option value="minus">ー</option>
-                <option value="plus">✕</option>
-                <option value="random">ランダム</option>
-              </select>
-            </label>
+            <div className="choice-arithmetic-container">
+              <Box sx={{ minWidth: 100 }}>
+                <FormControl fullWidth>
+                  <InputLabel id="choice-arithmetic">四則演算</InputLabel>
+                  <Select
+                    labelId="choice-arithmetic"
+                    id="arithmetic-select"
+                    value={arithmetic}
+                    label="arithmetic"
+                    onChange={handleChange}
+                  >
+                    <MenuItem value={"+"}>+</MenuItem>
+                    <MenuItem value={"-"}>−</MenuItem>
+                    <MenuItem value={"×"}>×</MenuItem>
+                  </Select>
+                </FormControl>
+              </Box>
+            </div>
           </div>
           <Button variant='contained' onClick={checkUpper} className='generate-button'>生成</Button>
         </div>
