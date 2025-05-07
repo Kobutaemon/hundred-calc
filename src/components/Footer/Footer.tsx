2daf70c86009ce310ab2e038317bb5b9224a04d1
Kobutaemon <ceti1241010@gn.iwasaki.ac.jp>
2025-05-07 13:03:15 +0900

    Prettierによる整形

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,14 +1,21 @@
-import "./Footer.css"
-import { Button } from '@mui/material';
+import "./Footer.css";
+import { Button } from "@mui/material";
 
 function Footer() {
   return (
     <div>
-      <footer className='no-print'>
-        <Button variant='outlined' onClick={() => {window.location.href="/"}}>トップページへ戻る</Button>
+      <footer className="no-print">
+        <Button
+          variant="outlined"
+          onClick={() => {
+            window.location.href = "/";
+          }}
+        >
+          トップページへ戻る
+        </Button>
       </footer>
     </div>
-  )
+  );
 }
 
-export default Footer 
+export default Footer;
